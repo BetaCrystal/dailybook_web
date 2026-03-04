ebbd5011726d92a7ac639171e5bc21b86e07b1ef
Eva Valenti <b.beta.crystal@gmail.com>
2026-03-04 17:18:56 +0100

    Réarrangement du menu profil

--- src/app/(private)/profile/profile-informations/ProfileInformationsContent.tsx
+++ src/app/(private)/profile/profile-informations/ProfileInformationsContent.tsx
@@ -5,9 +5,20 @@ import { ProfileSetting } from "@/components/ui/profile-setting";
 import { Button } from "@/components/ui";
 import { useUserContext } from "@/context/UserContext";
 import Link from "next/link";
+import { signOut } from 'firebase/auth';
+import { auth } from '@/lib/firebase';
 
 export default function ProfileInformationsContent(){
     const user = useUserContext();
+    const handleLogout = async () => {
+                    try {
+                            await signOut(auth);
+                            await fetch('/api/session/logout', { method: 'POST' });
+                            window.location.href = '/login';
+                    } catch (error) {
+                            console.error('Erreur de déconnexion :', error);
+                    }
+            };
     return(
         <div className="flex justify-center items-center flex-col pt-12">
             <p className="gap-1.5 px-6 text-lg font-semibold mb-1">Informations</p>
@@ -22,7 +33,7 @@ export default function ProfileInformationsContent(){
                 <ProfileSetting>Support utilisateur</ProfileSetting>
                 <ProfileSetting>Passer en mode sombre</ProfileSetting>
                 <ProfileSetting>Supprimer mon compte</ProfileSetting>
-                <ProfileSetting>Déconnexion</ProfileSetting>
+                <ProfileSetting onClick={handleLogout}>Déconnexion</ProfileSetting>
 
             </div>
             <Button>
--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -12,13 +12,21 @@ import {
         DropdownMenuLabel,
         DropdownMenuSeparator,
         DropdownMenuTrigger,
+        DropdownMenuSubTrigger,
+        DropdownMenuSubContent,
+        DropdownMenuSub,
 } from '@/components/ui/dropdown-menu';
 import { signOut } from 'firebase/auth';
 import { auth } from '@/lib/firebase';
 import { LogOut, Settings, User } from 'lucide-react';
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
+import { ProfileDisplay, StatusText } from "@/components/profile/ProfileDisplay";
+import { useUserContext } from "@/context/UserContext";
 
 export default function UserButton() {
+    const pathname = usePathname();
+        const user = useUserContext();
 
         const handleLogout = async () => {
                 try {
@@ -43,20 +51,65 @@ export default function UserButton() {
 
                                 <DropdownMenuSeparator />
 
-                                <Link href="/profile/profile-informations">
                                 <DropdownMenuItem>
-
-                                        <User className="mr-2 h-4 w-4" />
-                                        <span>Profil</span>
-
+                                    <DropdownMenuSub>
+                                        <DropdownMenuSubTrigger className={`{'text-sm font-medium text-primary'}`}><User className="mr-2 h-4 w-4" />Profil</DropdownMenuSubTrigger>
+                                        <DropdownMenuSubContent className="z-50">
+                                            <DropdownMenuItem className="grid gap-2 w-48">
+                                                <ProfileDisplay
+                                                user={user.user}
+                                                premiumStat={StatusText(user.user?.isPremium)}>
+                                                </ProfileDisplay>
+                                                    <Link
+                                                        href="/performances/objectives"
+                                                        className={`text-sm rounded-sm px-2 py-1.5 hover:bg-accent hover:text-primary ${pathname === '/performances/objectifs' && 'bg-accent text-primary'}`}
+                                                    >
+                                                    <DropdownMenuItem>Changer le nom</DropdownMenuItem>
+                                                    </Link>
+                                                    <Link
+                                                        href="/performances/statistiques"
+                                                        className={`text-sm rounded-sm px-2 py-1.5 hover:bg-accent hover:text-primary ${pathname === '/performances/statistiques' && 'bg-accent text-primary'}`}
+                                                    >
+                                                    <DropdownMenuItem>Changer le mot de passe</DropdownMenuItem>
+                                                    </Link>
+                                            </DropdownMenuItem>
+                                        </DropdownMenuSubContent>
+                                    </DropdownMenuSub>
                                 </DropdownMenuItem>
-                                </Link>
-                                <Link href="/profile/profile-settings">
                                 <DropdownMenuItem>
-                                        <Settings className="mr-2 h-4 w-4" />
-                                        <span>Paramètres</span>
+                                    <DropdownMenuSub>
+                                        <DropdownMenuSubTrigger className={`{'text-sm font-medium text-primary'}`}><Settings className="mr-2 h-4 w-4" />Paramètres</DropdownMenuSubTrigger>
+                                            <DropdownMenuSubContent className="z-50">
+                                                <DropdownMenuItem className="grid gap-2 w-48">
+                                                    <Link
+                                                        href="/performances/objectives"
+                                                        className={`text-sm rounded-sm px-2 py-1.5 hover:bg-accent hover:text-primary ${pathname === '/performances/objectifs' && 'bg-accent text-primary'}`}
+                                                    >
+                                                    <DropdownMenuItem>Gérer mon compte</DropdownMenuItem>
+                                                    </Link>
+                                                    <Link
+                                                        href="/performances/statistiques"
+                                                        className={`text-sm rounded-sm px-2 py-1.5 hover:bg-accent hover:text-primary ${pathname === '/performances/statistiques' && 'bg-accent text-primary'}`}
+                                                    >
+                                                    <DropdownMenuItem>Support utilisateurs</DropdownMenuItem>
+                                                    </Link>
+                                                    <Link
+                                                        href="/performances/statistiques"
+                                                        className={`text-sm rounded-sm px-2 py-1.5 hover:bg-accent hover:text-primary ${pathname === '/performances/statistiques' && 'bg-accent text-primary'}`}
+                                                    >
+                                                    <DropdownMenuItem>Passer en mode sombre</DropdownMenuItem>
+                                                    </Link>
+                                                    <Link
+                                                        href="/performances/statistiques"
+                                                        className={`text-sm rounded-sm px-2 py-1.5 hover:bg-accent hover:text-primary ${pathname === '/performances/statistiques' && 'bg-accent text-primary'}`}
+                                                    >
+                                                    <DropdownMenuItem>Supprimer mon compte</DropdownMenuItem>
+                                                    </Link>
+                                            </DropdownMenuItem>
+                                        </DropdownMenuSubContent>
+                                    </DropdownMenuSub>
                                 </DropdownMenuItem>
-                                </Link>
+
 
                                 <DropdownMenuSeparator />
 
