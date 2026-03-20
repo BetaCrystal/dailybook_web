fd051936c21e16cb744ea7b9732eb476193ba960
Eva Valenti <b.beta.crystal@gmail.com>
2026-03-20 09:14:15 +0100

    bug 404 non réparé

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -10,7 +10,7 @@ type UserContextType = {
   user: User | undefined;
   isLoading: boolean;
   isError: any;
-  updateUser: (uid: string, user: Partial<User>) => Promise<void>;
+  updateUser: (id: string, user: Partial<User>) => Promise<void>;
   refresh: () => void;
 };
 
@@ -19,9 +19,9 @@ const UserContext = createContext<UserContextType | undefined>(undefined);
 export function UserProvider({ children }: { children: React.ReactNode }) {
   const { user, isLoading, isError, mutate } = useCurrentUser();
 
-  const updateUser = async (uid: string, user: Partial<User>) => {
+  const updateUser = async (id: string, user: Partial<User>) => {
       try {
-        await userService.updateUser(uid, user);
+        await userService.updateUser(id, user);
         await mutate();
       } catch (err: any) {
         Sentry.captureException(err);
